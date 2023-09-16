a12d89060ee00d362961d30b1bb63c70ac3f946a
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-09-15 19:21:48 -0500

    sync server side linker distance to connecting clients

---
+++ ModSystem/SyncClientPacket.cs
@@ -0,0 +1,8 @@
+﻿using ProtoBuf;
+
+namespace TranslocatorEngineering.ModSystem {
+    [ProtoContract(ImplicitFields = ImplicitFields.AllPublic)]
+    public class SyncClientPacket {
+        public int MaximumLinkRange;
+    }
+}
